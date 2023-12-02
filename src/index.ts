114ae66a357e3ccd07b4ab9638995066ec21a840
Klaus Nascimento <klausnascimento@gmail.com>
2023-12-02 17:22:47 -0300

    import express, import dotenv, config simples do dotenv, peguei a port do file .env, definir uma rota, subo o servescutando a port que é 8090, passei o log pra avisar que o servidor está UP

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('started')
+
+import express from "express"; //importou o express
+
+import dotenv from "dotenv"; //importou o dotenv
+
+dotenv.config() //configuração simples do dotenv
+
+const PORT = process.env.PORT || 8080 //peguei a port do arquivo .env
+
+const app = express() //pego o expess
+
+app.get('/', (request, response) => { //defino uma rota (barra /)
+  response.send("Server up KLAUS-")
+})
+
+app.listen(PORT, () => { //subo o servidor, escutando a porta que é 8090
+  console.log(`server running in port ${PORT}`) //passo um log pra avisar que o servidor está UP
+})
